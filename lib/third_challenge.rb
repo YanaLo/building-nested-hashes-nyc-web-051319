2b6cba26111b5f9faf462ba7f44d6f3c32f55895
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-10 07:14:14 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,7 +11,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => [{name:"Benvolio" age:"17" attitude:"worried"}
-           {name:"" age:"" attitude:""}  
+           {name:"Mercutio" age:"18" attitude:""}  
       ]
    }, 
    :capulet => {
